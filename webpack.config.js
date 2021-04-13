44c5be25d1694865a6f975f83d46396fabe749d7
Daniel Vasylenko <danielspr3@gmail.com>
2021-04-13 21:03:49 +0200

    chore: bump webpack and cleanup webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -34,12 +34,7 @@ module.exports = {
           context: '../',
           from: 'src/icons',
           to: 'icons/'
-        },
-        // {
-        //   context: '../',
-        //   to: '[name].[ext]',
-        //   from: 'node_modules/webextension-polyfill/dist/browser-polyfill.min.js'
-        // }
+        }
       ]
     })
   ]
